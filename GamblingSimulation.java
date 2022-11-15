be9d10257c2fdb7f3f6aaac1e629819c96d8d675
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-16 02:44:03 +0530

    add continue the game function

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 import javax.swing.LookAndFeel;
 
@@ -26,41 +27,54 @@ public class GamblingSimulation {
 		int totalAmount = 0;
 		ArrayList<Integer> winNumberList = new ArrayList<>();
 		ArrayList<Integer> loosNumberList = new ArrayList<>();
+		char choice;
+		Scanner scanObj = new Scanner(System.in);
+		do {
 
-		for (int i = 1; i <= day; i++) {
-			int winStake = 0;
-			int looseStake = 0;
-			int cash = 0;
-			int gameNumber = 0;
-			while (winStake < oneDayGamestake * 50 / 100 && looseStake < oneDayGamestake * 50 / 100) {
-				int isGameWinOrLoose = randomCheckGame();
-				switch (isGameWinOrLoose) {
-				case 0:
-					gameNumber++;
-					looseStake = looseStake + perGameBet;
+			for (int i = 1; i <= day; i++) {
+				int winStake = 0;
+				int looseStake = 0;
+				int cash = 0;
+				int gameNumber = 0;
+				while (winStake < oneDayGamestake * 50 / 100 && looseStake < oneDayGamestake * 50 / 100) {
+					int isGameWinOrLoose = randomCheckGame();
+					switch (isGameWinOrLoose) {
+					case 0:
+						gameNumber++;
+						looseStake = looseStake + perGameBet;
 
-					cash = oneDayGamestake - perGameBet;
-					break;
-				case 1:
+						cash = oneDayGamestake - perGameBet;
+						break;
+					case 1:
 
-					gameNumber++;
-					winStake = winStake + perGameBet;
+						gameNumber++;
+						winStake = winStake + perGameBet;
 
-					cash = oneDayGamestake + perGameBet;
-					break;
+						cash = oneDayGamestake + perGameBet;
+						break;
+
+					}
 
 				}
+				winNumberList.add(winStake);
+				loosNumberList.add(looseStake);
+				System.out.println("Total Game play in One Day : " + gameNumber);
+				
+				System.out.println("Case collection of Game Day " + i + ": " + cash);
+				System.out.println("----------------------------");
 
 			}
-			winNumberList.add(winStake);
-			loosNumberList.add(looseStake);
+			System.out.println("number of win :" + winNumberList);
+			System.out.println("number of loos: " + loosNumberList);
+			System.out.println("Total Amount in month: " + totalAmount);
+			
 
-		}
-		System.out.println("number of win :" + winNumberList);
-		System.out.println("number of loos: " + loosNumberList);
+			findLuckiestDay(winNumberList);
+			findUnluckiestDay(loosNumberList);
 
-		findLuckiestDay(winNumberList);
-		findUnluckiestDay(loosNumberList);
+			System.out.println("would you like to continue playing next month ?");
+			choice = scanObj.next().charAt(0);
+		} while ((choice == 'y') || (choice == 'Y'));
 
 	}
 
