0e74aa5f083d8c429d920cc792371fd9c6e7ba9d
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-15 16:51:15 +0530

    apply condition loop  for resign  for the day

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -9,7 +9,8 @@ public class GamblingSimulation {
 		System.out.println("Gambler start with a stake 100");
 
 		GamblingSimulation gambling = new GamblingSimulation();
-		gambling.playGame();
+		//gambling.playGame();
+	gambling.oneDayGame();
 	}
 
 	public void playGame() {
@@ -33,7 +34,41 @@ public class GamblingSimulation {
 			oneDayGamestake = oneDayGamestake + winStack;
 			break;
 		}
-		System.out.println("Game Stack : " + oneDayGamestake);
+		System.out.println("Game Stake : " + oneDayGamestake);
 
+	}
+	public void oneDayGame() {
+		int winStake = 0;
+		int looseStake = 0;
+
+		int cash = oneDayGamestake;
+		
+		while(winStake < oneDayGamestake*50/100 && looseStake < oneDayGamestake*50/100) {
+			int isGameWinOrLoose = (int) (Math.random() * 2);
+			
+			switch (isGameWinOrLoose) {
+			case 0:
+				System.out.println("Gambler loose the game");
+				gameNumber++;
+				looseStake = looseStake + perGameBet;
+				System.out.println(looseStake);
+				cash = oneDayGamestake - looseStake;
+				break;
+			case 1:
+				System.out.println("Gambler win the game");
+				gameNumber++;
+				winStake = winStake + perGameBet;
+				System.out.println(winStake);
+				cash = oneDayGamestake + winStake;
+				break;
+		}
+			System.out.println("Game Stack : " + cash);
+			System.out.println("Game number : " + gameNumber);
+			System.out.println("Loosing game:" +looseStake);
+			System.out.println("wining game : " + winStake);
+		}
+			
+		
+		
 	}
 }
